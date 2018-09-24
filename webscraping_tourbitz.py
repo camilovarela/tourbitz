9e4f32363f6dbe3742028ac81f5bb26b819eae6e
Camilo Varela <cvarela@grupodot.com>
2018-09-24 11:55:17 -0500

    Adding try/catch

--- webscraping_tourbitz.py
+++ webscraping_tourbitz.py
@@ -29,51 +29,53 @@ try:
 except TimeoutException:
     print("Timed out!")
 
-
-element = browser.find_element_by_xpath("//div[@class='activities-show-more load-more']")
-process = True
-
-while process:
-
-	browser.execute_script("arguments[0].click();", element)
-	browser.execute_script("arguments[0].scrollIntoView();", element)
-	
-	time.sleep(0.6)
-	
+try:
 	element = browser.find_element_by_xpath("//div[@class='activities-show-more load-more']")
-	child = browser.find_element_by_xpath("//span[@class='btn btn-cta btn-small']")
-	
-	if child.get_attribute("style") == 'display: none;':
-	
-		process = False
+	process = True
+
+	while process:
+
+		browser.execute_script("arguments[0].click();", element)
+		browser.execute_script("arguments[0].scrollIntoView();", element)
 		
-		# find_elements_by_xpath - Returns an array of selenium objects.
-		articles_element = browser.find_elements_by_xpath("//a[@class='activity-card-link']")
+		time.sleep(0.6)
 		
-		step = 1
-		for article in articles_element:
-			
-			# Go to detail page
-			detail.get("" + article.get_attribute("href"))
-			
-			about_list = detail.find_elements_by_xpath("//div[contains(@class, 'keydetails-benefit key-details-item')]//div[contains(@class, 'label')]")
-			
-			tour = TourPlan()
-			tour.title = detail.find_element_by_xpath("//h1[@class='activity-title']").text
-			tour.overview = detail.find_element_by_xpath("//section[@class='overview long-txt']//div[@class='content']").text
-			tour.price = detail.find_element_by_xpath("//p[contains(@class, 'price')]//strong[@class='price-actual']").text
-			tour.image = detail.find_element_by_xpath("//div[@class='activity-header-image-contain']").get_attribute("data-background")
+		element = browser.find_element_by_xpath("//div[@class='activities-show-more load-more']")
+		child = browser.find_element_by_xpath("//span[@class='btn btn-cta btn-small']")
 		
-			jsonStr = '{"title":"' + tour.title + '", "overview":"' + tour.overview + '", "price":"' + tour.price + '", "image":"' + tour.image + '", "about":['
+		if child.get_attribute("style") == 'display: none;':
+		
+			process = False
 			
-			for i in range(1, len(about_list)):
-				if i > 1:
-					jsonStr = jsonStr + ','
-				jsonStr = jsonStr + '"' + about_list[i].text + '"'
-			jsonStr = jsonStr + ']}'
-			print(jsonStr)
+			# find_elements_by_xpath - Returns an array of selenium objects.
+			articles_element = browser.find_elements_by_xpath("//a[@class='activity-card-link']")
 			
-			time.sleep(10)
+			step = 1
+			for article in articles_element:
+				
+				# Go to detail page
+				detail.get("" + article.get_attribute("href"))
+				
+				about_list = detail.find_elements_by_xpath("//div[contains(@class, 'keydetails-benefit key-details-item')]//div[contains(@class, 'label')]")
+				
+				tour = TourPlan()
+				tour.title = detail.find_element_by_xpath("//h1[@class='activity-title']").text
+				tour.overview = detail.find_element_by_xpath("//section[@class='overview long-txt']//div[@class='content']").text
+				tour.price = detail.find_element_by_xpath("//p[contains(@class, 'price')]//strong[@class='price-actual']").text
+				tour.image = detail.find_element_by_xpath("//div[@class='activity-header-image-contain']").get_attribute("data-background")
+			
+				jsonStr = '{"title":"' + tour.title + '", "overview":"' + tour.overview + '", "price":"' + tour.price + '", "image":"' + tour.image + '", "about":['
+				
+				for i in range(1, len(about_list)):
+					if i > 1:
+						jsonStr = jsonStr + ','
+					jsonStr = jsonStr + '"' + about_list[i].text + '"'
+				jsonStr = jsonStr + ']}'
+				print(jsonStr)
+				
+				time.sleep(10)
+except TimeoutException:
+	fail = True
 
 browser.quit()
 detail.quit()
